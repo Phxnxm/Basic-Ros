cd7a43dce8eb78249dd2071ffcfc022b674c79ec
Phxnxm <120149466+Phxnxm@users.noreply.github.com>
2025-04-10 11:33:45 +0700

    publisher.py

--- publisher.py
+++ publisher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
+
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
@@ -10,6 +11,7 @@ def talker():
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
+        
 if __name__ == '__main__':
     try:
         talker()
